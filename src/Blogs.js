d416cc93739669c098a559718f615e60a49358db
Priti Sherekar <63558030+PS0108@users.noreply.github.com>
2020-04-23 14:40:58 -0400

    In page opening of blog page

--- src/Blogs.js
+++ src/Blogs.js
@@ -11,7 +11,7 @@ class Blogs extends Component{
         return (
             <div className="column left">
                 <div className="innerrow">
-                    <p><h1><em><NavLink to="/BlogOne" className="ul_c" target="_blank">What Is Latency and Why Is it Important?</NavLink></em></h1></p>
+                    <p><h1><em><NavLink to="/BlogOne" className="ul_c">What Is Latency and Why Is it Important?</NavLink></em></h1></p>
                     <p>04/15/2020</p>
                     <div className="innercols">
                         <p>Latency is simply defined as the time taken for one operation to happen.Even Google and Amazon can’t process data instantly—here’s how to combat latency in your real-time application</p>
@@ -21,7 +21,7 @@ class Blogs extends Component{
                     </div>
                 </div>
                 <div className="innerrow">
-                    <p><h1><em><NavLink to="/BlogTwo" className="ul_c" target="_blank">How Is Latency Characterized?</NavLink></em></h1></p>
+                    <p><h1><em><NavLink to="/BlogTwo" className="ul_c">How Is Latency Characterized?</NavLink></em></h1></p>
                     <p>04/15/2020</p>
 
                     <div className="innercols">
@@ -34,7 +34,7 @@ class Blogs extends Component{
                 </div>
 
                 <div className="innerrow">
-                        <p><h1><em><NavLink to="/BlogThree" className="ul_c" target="_blank">What Contributes to Latency?</NavLink></em></h1></p>
+                        <p><h1><em><NavLink to="/BlogThree" className="ul_c">What Contributes to Latency?</NavLink></em></h1></p>
                         <p>04/15/2020</p>
                         <div className="innercols">
                             <p>Often the hardest part, though, is identifying the sources of latency in your application and subsequently eliminating them. If you can’t remove them entirely, there are steps you can take to reduce or manage their consequences.</p>
